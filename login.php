e3c6f668b1bfd8832058d46669fa3ef15a08c8e0
Hang Rithy <hangrithy001@gmail.com>
2025-03-10 15:21:06 +0700

    delete add and home php but added login php

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div class="name">
+            <label for="txtUserName">Username</label>
+            <input type="text" name="txtUserName" id="txtUserName">
+        </div>
+
+        <div class="password">
+            <label for="txtPassword">Password</label>
+            <input type="text" name="txtPassword" id="txtPassword">
+        </div>
+    </form>
+</body>
+</html>
