4c1fe34537fc91832866930a81d366d506fe61d6
Hang Rithy <hangrithy001@gmail.com>
2025-03-10 14:32:46 +0700

    add new php

---
+++ add.php
@@ -0,0 +1,37 @@
+<?php
+if (isset($_POST["buttonSubmit"])) {
+    $fileNames = $_FILES["fileUploading"]["name"];
+    $sources = $_FILES["fileUploading"]["tmp_name"];
+    
+
+    echo $sources;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Products</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <div class="name">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name">
+        </div>
+
+
+        <div class="photo">
+            <input type="file" multiple name="fileUploading[]" id="fileUploading[]">
+            <input type="submit" value="SUBMIT" name="buttonSubmit">
+        </div>
+
+        
+    </form>
+</body>
+
+</html>
