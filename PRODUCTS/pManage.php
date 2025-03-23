489ee86eaa94f5bcfd98334d805d12f473e937f5
Rithy Hang <hangrithy002@gmail.com>
2025-03-23 13:00:54 +0700

    manage product

--- PRODUCTS/pManage.php
+++ PRODUCTS/pManage.php
@@ -1,3 +1,12 @@
-<?php
-    echo "Product Manage";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Product</title>
+</head>
+<body>
+    <a href="pAdd.php">Add Products</a> <br>
+    <a href="pUpdate.php">Update Product</a>
+</body>
+</html>
